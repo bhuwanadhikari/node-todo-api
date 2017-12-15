9130d0fcd36dd224683b9e8b97d9f56d39e73928
Bhuwan Adhikari <luminousbhuwan@gmail.com>
2017-12-15 22:22:06 +0545

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 //var mongoose = require('./db/mongoose').mongoose
 var {mongoose} = require('./db/mongoose') // identical to the line 1
@@ -8,20 +9,65 @@ var {User} = require('./models/user');
 
 
 var app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.json());//to coordinate with the requests in json way
 
+//POST request
 app.post('/todos', (req, res)=>{
     var todo = new Todo({
         text:req.body.text
     });
-    
+
     todo.save().then((doc)=>{
         res.send(doc);
-    },(err)=>{
-    	res.send("cannot be added to the database",err);
+    },(e)=>{
+    	res.status(400).send(e);
     });
 });
+
+//GET request
+app.get('/todos', (req, res)=>{
+  Todo.find().then((todos)=>{
+    res.send({todos});
+  },(e)=>{
+    res.status(400).send(e);
+  });
+});
+
+
+
+//Passing into the params
+app.get('/todos/:id', (req,res)=>{
+  var id = req.params.id;
+
+  //terminating if id is not valid
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo)=>{
+    //terminating if id doesn't exist
+    if(!todo){
+      return res.status(404).send();
+    }
+    //sending the todo if id is found
+    res.send({todo});
+  }).catch((e)=>{
+    res.status(400).send();
+  });
+});
+
 
 app.listen(3000, ()=>{
     console.log("server is up at port:3000");
 });
+
+
+
+
+
+
+
+
+
+
+//
